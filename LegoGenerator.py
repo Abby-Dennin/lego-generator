f93e11f4ed125c261f3f402e2439f68e8a02cdf5
Abby-Dennin <71346138+Abby-Dennin@users.noreply.github.com>
2022-12-18 21:31:15 -0500

    Update LegoGenerator.py
    Added ability to smooth lego and also make it a lego plate instead of a brick for height

--- LegoGenerator.py
+++ LegoGenerator.py
@@ -5,29 +5,30 @@ class LegoBrick(object):
     def __init__(self):
         self.brick = None
 
-    def create_brick(self, length, width, color, plate=False):
-        size_y = 0.32 if plate else 0.96
-        size_x = width * 0.8
-        size_z = length * 0.8
+    def create_brick(self, brick_data):
+        size_y = 0.32 if brick_data["plate"] else 0.96
+        size_x = brick_data["width"] * 0.8
+        size_z = brick_data["length"] * 0.8
 
-        brick = cmds.polyCube(h=size_y, w=size_x, d=size_z, sx=width, sz=length)
+        brick = cmds.polyCube(h=size_y, w=size_x, d=size_z, sx=brick_data["width"], sz=brick_data["length"])
 
-        for i in range(width):
-            for j in range(length):
-                nub = cmds.polyCylinder(r=0.25, h=0.2)
-                cmds.move(size_y / 2.0 + 0.1, moveY=True, a=True)
-                cmds.move((-size_x / 2.0 + (i + 0.5) * 0.8), moveX=True, a=True)
-                cmds.move((-size_z / 2.0 + (j + 0.5) * 0.8), moveZ=True, a=True)
+        if (not brick_data["smooth"]):
+            for i in range(brick_data["width"]):
+                for j in range(brick_data["length"]):
+                    nub = cmds.polyCylinder(r=0.25, h=0.2)
+                    cmds.move(size_y / 2.0 + 0.1, moveY=True, a=True)
+                    cmds.move((-size_x / 2.0 + (i + 0.5) * 0.8), moveX=True, a=True)
+                    cmds.move((-size_z / 2.0 + (j + 0.5) * 0.8), moveZ=True, a=True)
 
-                brick = cmds.polyCBoolOp(brick, nub, op=1, ch=False)
+                    brick = cmds.polyCBoolOp(brick, nub, op=1, ch=False)
 
         brick_hole = cmds.polyCube(h=size_y, w=size_x - 0.12 * 2, d=size_z - 0.12 * 2, sx=1, sz=1)
         cmds.move(-0.1, moveY=True)
 
         brick = cmds.polyCBoolOp(brick, brick_hole, op=2, ch=False)
 
-        for i in range(width - 1):
-            for j in range(length - 1):
+        for i in range(brick_data["width"] - 1):
+            for j in range(brick_data["length"] - 1):
                 inner_nub = cmds.polyCylinder(r=0.3255, h=size_y - 0.2, sx=10)
                 inner_nub_hole = cmds.polyCylinder(r=0.25, h=size_y, sx=10)
                 inner_nub = cmds.polyCBoolOp(inner_nub, inner_nub_hole, op=2, ch=False)
@@ -38,9 +39,9 @@ class LegoBrick(object):
 
                 brick = cmds.polyCBoolOp(brick, inner_nub, op=1, ch=False)
 
-        print(brick)
-        self.apply_material(color)
         self.brick = brick
+        self.apply_material(brick_data["color"])
+        
 
     def apply_material(self, color):
         if cmds.objExists(self.brick[0]):
