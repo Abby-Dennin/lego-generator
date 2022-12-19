fa45b7a875957e31e7d15f2c1a656c9bb54ccac0
Abby-Dennin <71346138+Abby-Dennin@users.noreply.github.com>
2022-12-18 21:32:06 -0500

    Updated palette and added checks
    Checks are to toggle smooth and plate bricks

--- LegoUI.py
+++ LegoUI.py
@@ -1,6 +1,23 @@
 from maya import cmds
 from LegoGenerator import LegoBrick
 
+LEGO_COLORS = [
+    {"r": 0.79, "g": 0.10, "b": 0.04}, # red
+    {"r": 0.99, "g": 0.54, "b": 0.10}, # orange
+    {"r": 0.95, "g": 0.80, "b": 0.22}, # yellow
+    {"r": 0.14, "g": 0.47, "b": 0.25}, # green
+    {"r": 0.0, "g": 0.33, "b": 0.75}, # blue
+    {"r": 0.51, "g": 0.0, "b": 0.48}, #purple
+    {"r": 0.89, "g": 0.80, "b": 0.62}, # tan 
+    {"r": 0.58, "g": 0.54, "b": 0.45}, # dark tan
+    {"r": 0.35, "g": 0.16, "b": 0.07}, # brown
+    {"r": 1.0, "g": 1.0, "b": 1.0}, # white
+    {"r": 0.61, "g": 0.63, "b": 0.62}, # light grey
+    {"r": 0.34, "g": 0.35, "b": 0.34}, # dark grey
+    {"r": 0.02, "g": 0.07, "b": 0.11} # black
+]
+
+
 class LegoWindow(object):
     """
     This class builds a window for the lego generator.
@@ -10,6 +27,8 @@ class LegoWindow(object):
         self.window_name = "LegoWindow"
         self.length_slider = None
         self.width_slider = None
+        self.plate_check = None
+        self.smooth_check = None
         self.create_btn = None
         self.palette = None
 
@@ -23,8 +42,8 @@ class LegoWindow(object):
 
     def build(self):
         column = cmds.columnLayout("Brick")
-        self.width_slider = cmds.intSliderGrp("brickWidth", label="Brick Width", field=True, min=1, max=10, v=4)
-        self.length_slider = cmds.intSliderGrp("brickLength", label="Brick Length", field=True, min=1, max=10, v=2)
+        self.width_slider = cmds.intSliderGrp("brickWidth", label="Brick Width", field=True, min=1, max=16, v=4)
+        self.length_slider = cmds.intSliderGrp("brickLength", label="Brick Length", field=True, min=1, max=16, v=2)
 
         cmds.separator(h=10, style='none')
 
@@ -33,32 +52,39 @@ class LegoWindow(object):
 
         cmds.setParent(column)
 
+        self.plate_check = cmds.checkBox(label='Plate')
+        self.smooth_check = cmds.checkBox(label='Smooth')
+
         cmds.separator(h=10, style='none')
 
         self.create_btn = cmds.button(label="Create Brick", c=self.create_brick)
 
     def create_brick(self, *args):
-        brick_length = cmds.intSliderGrp(self.length_slider, q=True, v=True)
-        brick_width = cmds.intSliderGrp(self.width_slider, q=True, v=True)
-        brick_color = self.get_color()
-        brick = LegoBrick().create_brick(brick_length, brick_width, brick_color)
+        # brick_length = cmds.intSliderGrp(self.length_slider, q=True, v=True)
+        # brick_width = cmds.intSliderGrp(self.width_slider, q=True, v=True)
+        # brick_color = cmds.palettePort(self.palette, q=True, rgb=True)
+        # brick_plate = cmds.checkBox(self.plate_check, q=True, v=True)
+        # brick_smooth = cmds.checkBox(self.smooth_check, q=True, v=True)
+
+        brick_data = {
+            "length": cmds.intSliderGrp(self.length_slider, q=True, v=True),
+            "width": cmds.intSliderGrp(self.width_slider, q=True, v=True),
+            "color": cmds.palettePort(self.palette, q=True, rgb=True),
+            "plate": cmds.checkBox(self.plate_check, q=True, v=True),
+            "smooth": cmds.checkBox(self.smooth_check, q=True, v=True),
+        }
+
+        brick = LegoBrick().create_brick(brick_data)
 
     def get_color_palette(self):
-        palette = cmds.palettePort('palette', dim=(8, 1), cc=self.get_color)
-        cmds.palettePort('palette', edit=True, rgb=(0, 0.79, 0.10, 0.04))
-        cmds.palettePort('palette', edit=True, rgb=(1, 0.99, 0.54, 0.10))
-        cmds.palettePort('palette', edit=True, rgb=(2, 0.95, 0.80, 0.22))
-        cmds.palettePort('palette', edit=True, rgb=(3, 0.14, 0.47, 0.25))
-        cmds.palettePort('palette', edit=True, rgb=(4, 0.0, 0.33, 0.75))
-        cmds.palettePort('palette', edit=True, rgb=(5, 0.51, 0.0, 0.48))
-        cmds.palettePort('palette', edit=True, rgb=(6, 1.0, 1.0, 1.0))
-        cmds.palettePort('palette', edit=True, rgb=(7, 0.02, 0.07, 0.11))
+        palette = cmds.palettePort('palette', dim=(13, 1))
+        c = 0
+        for color in LEGO_COLORS:
+            cmds.palettePort('palette', edit=True, rgb=(c, color["r"], color["g"], color["b"]))
+            c = c + 1
+     
         cmds.palettePort('palette', edit=True, redraw=True)
         self.palette = palette
-
-    def get_color(self):
-        if self.palette:
-            return cmds.palettePort(self.palette, query=True, rgb=True)
 
     def close(self, *args):
         cmds.deleteUI(self.window_name)
